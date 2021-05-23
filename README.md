6abc92cda4421713be79722b1cc591041e69fa87
Amatke31 <amatke@maftert.com>
2021-05-24 00:48:57 +0800

    first time

---
+++ README.md
@@ -0,0 +1,24 @@
+# vue-MamicPlayer
+
+[![npm](https://img.shields.io/npm/v/vue-mamicplayer.svg?style=flat-square)](https://www.npmjs.com/package/vue-mamicplayer)
+[![npm](https://img.shields.io/npm/l/vue-mamicplayer.svg?style=flat-square)](https://github.com/MoePlayer/APlayer/blob/master/LICENSE)
+[![npm](https://img.shields.io/npm/dt/vue-mamicplayer.svg?style=flat-square)](https://www.npmjs.com/package/vue-mamicplayer)
+
+## Introduction
+
+MamicPlayer is Nexmamic platform lovely Player  
+There are two modes of full screen and suspension
+
+([Nexmamic](https://music.maftert.com) is a music creation sharing platform in China)
+
+**MamicPlayer Supports:**
+
+
+
+Let's have a look:
+
+![](public/player.png)
+
+![](public/playermax.png)
+
+![](public/playermax-p.png)
---
+++ webpack.production.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    publicPath: '/dist/',
+    library: 'vue-MamicPlayer',
+    libraryTarget: 'umd',
+    globalObject: 'this'
+  },
+  externals: {
+    vue: 'vue',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: ['vue-loader'],
+        include: path.resolve(__dirname, 'src'),
+      },
+      {
+        test: /\.(js|jsx)?$/,
+        use: ['babel-loader'],
+        include: path.resolve(__dirname, 'src'),
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        include: path.resolve(__dirname, 'src'),
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader'],
+        include: path.resolve(__dirname, 'src'),
+      },
+      {
+        test: /\.(eot|woff|otf|svg|ttf|woff2|appcache|mp3|mp4|pdf)(\?|$)/,
+        include: path.resolve(__dirname, 'src'),
+        use: ['file-loader?name=assets/[name].[ext]'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)(\?|$)/,
+        include: path.resolve(__dirname, 'src'),
+        use: ['url-loader?limit=8192&name=assets/[name].[ext]'],
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin(), new CleanWebpackPlugin()],
+  resolve: {
+    extensions: ['.js', '.vue', '.less', '.css'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+};
